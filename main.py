9b4b859aa3193188fac4bc2960f951c042c0495a
Zsolt Biro <zsolt.biro@accenture.com>
2019-10-09 22:33:19 +0300

    prepared UI for dtw distance calculation

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+# package used for reading files
 import os
+# package used for building user interface
 import tkinter as tk
 from tkinter import *
-
+# packages used for plotting figures
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# packages used for dynamic time warping calculations
+import numpy as np
+import dtw as dtw
+from dtw import *
 
 
 class Application:
     # variables used to draw figures
     x = []
     y = []
+    compare_x = []
+    compare_y = []
     number_of_events = []
+    compare_number_of_events = []
     # main window variable
     window = tk.Tk()
     # labels and drop down lists frame
@@ -25,19 +34,29 @@ class Application:
     folder_name = StringVar(mainframe)
     session_name = StringVar(mainframe)
     file_name = StringVar(mainframe)
+    compare_folder_name = StringVar(mainframe)
+    compare_session_name = StringVar(mainframe)
+    compare_file_name = StringVar(mainframe)
     # the list of options
     folder_list = os.listdir('e-BioDigit_DB/')
     folder_list.sort()
+    compare_folder_list = os.listdir('e-BioDigit_DB/')
+    compare_folder_list.sort()
     session_list = os.listdir('e-BioDigit_DB/' + folder_list[0])
     session_list.sort()
+    compare_session_list = os.listdir('e-BioDigit_DB/' + compare_folder_list[0])
+    compare_session_list.sort()
     number_list = os.listdir('e-BioDigit_DB/' + folder_list[0] + '/' + session_list[0])
     number_list.sort()
+    compare_number_list = os.listdir('e-BioDigit_DB/' + compare_folder_list[0] + '/' + compare_session_list[0])
+    compare_number_list.sort()
     popup_menu = OptionMenu(mainframe, file_name, *number_list)
+    compare_popup_menu = OptionMenu(mainframe, compare_file_name, *compare_number_list)
 
     def __init__(self):
         # set the main window title and size
         self.window.title("Number Representation")
-        self.window.geometry('690x540')
+        self.window.geometry('690x840')
         # add a grid frame
         self.mainframe.grid(column=0, row=0, sticky=(N, W))
         self.mainframe.columnconfigure(0, weight=1)
@@ -45,52 +64,93 @@ class Application:
         self.mainframe.config(bg='black')
         self.mainframe.pack(side=tk.TOP, anchor=tk.NW)
         # create the drop down lists
-        self.create_folder_optionmenu()
+        self.create_folder_option_menu()
+        self.create_comparison_menu()
         # link functions to drop down value changes
         self.folder_name.trace('w', self.database_folder_changed)
         self.session_name.trace('w', self.session_folder_changed)
         self.file_name.trace('w', self.txt_file_changed)
+        self.compare_folder_name.trace('w', self.compare_database_folder_changed)
+        self.compare_session_name.trace('w', self.compare_session_folder_changed)
+        self.compare_file_name.trace('w', self.compare_txt_file_changed)
         # start the main window
         self.window.mainloop()
 
     # create the top labels and menus
-    def create_folder_optionmenu(self):
+    def create_folder_option_menu(self):
         # set default option
         self.folder_name.set(self.folder_list[0])
         # Popup Menu1
-        Label(self.mainframe, text="Choose a File: ", fg='white', bg='black').grid(row=1, column=1)
+        Label(self.mainframe, text="Choose a File:", fg='white', bg='black').grid(row=1, column=1)
         popup_menu1 = OptionMenu(self.mainframe, self.folder_name, *self.folder_list)
         popup_menu1.config(background='black', width=5)
         popup_menu1.grid(row=1, column=2)
         # set default option
         self.session_name.set(self.session_list[0])
         # Popup Menu2
-        Label(self.mainframe, text=" Choose a Session: ", fg='white', bg='black').grid(row=1, column=3)
+        Label(self.mainframe, text="a Session:", fg='white', bg='black').grid(row=1, column=3)
         popup_menu2 = OptionMenu(self.mainframe, self.session_name, *self.session_list)
         popup_menu2.config(background='black', width=9)
         popup_menu2.grid(row=1, column=4)
         # set default option
         self.file_name.set(self.number_list[0])
         # Popup Menu3
-        Label(self.mainframe, text=" Choose a Number: ", fg='white', bg='black').grid(row=1, column=5)
+        Label(self.mainframe, text="Draw Number:", fg='white', bg='black').grid(row=1, column=5)
         self.popup_menu.config(background='black', width=17)
         self.popup_menu.grid(row=1, column=6)
         self.draw_figures()
+
+    def create_comparison_menu(self):
+        # set default option
+        self.compare_folder_name.set(self.compare_folder_list[0])
+        Label(self.mainframe, text="Choose a File:", fg='white', bg='black').grid(row=2, column=1)
+        # Popup Menu1
+        popup_menu1 = OptionMenu(self.mainframe, self.compare_folder_name, *self.compare_folder_list)
+        popup_menu1.config(background='black', width=5)
+        popup_menu1.grid(row=2, column=2)
+        # set default option
+        self.compare_session_name.set(self.compare_session_list[0])
+        # Popup Menu2
+        Label(self.mainframe, text="a Session:", fg='white', bg='black').grid(row=2, column=3)
+        popup_menu2 = OptionMenu(self.mainframe, self.compare_session_name, *self.compare_session_list)
+        popup_menu2.config(background='black', width=9)
+        popup_menu2.grid(row=2, column=4)
+        # set default option
+        self.compare_file_name.set(self.compare_number_list[0])
+        # Popup Menu3
+        Label(self.mainframe, text="Compare to:", fg='white', bg='black').grid(row=2, column=5)
+        self.compare_popup_menu.config(background='black', width=17)
+        self.compare_popup_menu.grid(row=2, column=6)
 
     # on folder popup_menu value change
     def database_folder_changed(self, *args):
         self.folder_name.set(self.folder_name.get())
         self.refresh_file_name_option_menu()
 
+    # on folder popup_menu value change
+    def compare_database_folder_changed(self, *args):
+        self.compare_folder_name.set(self.compare_folder_name.get())
+        self.compare_refresh_file_name_option_menu()
+
     # on session popup_menu value change
     def session_folder_changed(self, *args):
         self.session_name.set(self.session_name.get())
         self.refresh_file_name_option_menu()
 
+    # on session popup_menu value change
+    def compare_session_folder_changed(self, *args):
+        self.compare_session_name.set(self.compare_session_name.get())
+        self.compare_refresh_file_name_option_menu()
+
     # on file name popup_menu value change
     def txt_file_changed(self, *args):
         self.file_name.set(self.file_name.get())
         self.draw_figures()
+
+    # on file name popup_menu value change
+    def compare_txt_file_changed(self, *args):
+        self.compare_file_name.set(self.compare_file_name.get())
+        self.compare_read_file()
 
     # refresh the option menu, with file name
     def refresh_file_name_option_menu(self):
@@ -103,11 +163,22 @@ class Application:
         for number in self.number_list:
             self.popup_menu["menu"].add_command(label=number, command=lambda value=number: self.file_name.set(value))
 
+    # refresh the option menu, with file name
+    def compare_refresh_file_name_option_menu(self):
+        # reload file list
+        self.compare_number_list = os.listdir('e-BioDigit_DB/' + self.compare_folder_name.get() + '/' + self.compare_session_name.get())
+        self.compare_number_list.sort()
+        self.compare_file_name.set(self.compare_number_list[0])
+        # refresh options
+        self.compare_popup_menu["menu"].delete(0, "end")
+        for number in self.compare_number_list:
+            self.compare_popup_menu["menu"].add_command(label=number, command=lambda value=number: self.compare_file_name.set(value))
+
     def draw_figure_number(self):
         self.figure = plt.Figure(figsize=(3, 4), dpi=100)
         self.ax = self.figure.add_subplot(111)
         self.graph_number = FigureCanvasTkAgg(self.figure, self.window)
-        self.graph_number.get_tk_widget().pack(side=tk.LEFT, anchor=tk.NW, fill=tk.Y)
+        self.graph_number.get_tk_widget().pack(side=tk.LEFT, anchor=tk.NW)
         self.ax.set_xlabel('X')
         self.ax.set_ylabel('Y')
         self.ax.set_xlim([0, 250])
@@ -133,6 +204,23 @@ class Application:
         self.ax.set_title('Y Coord Linear')
 
     def draw_figures(self):
+        self.read_file()
+        # remove graphs and redraw
+        self.graph_number.get_tk_widget().pack_forget()
+        self.graph_number.get_tk_widget().destroy()
+        self.draw_figure_number()
+        self.graph_x.get_tk_widget().pack_forget()
+        self.graph_x.get_tk_widget().destroy()
+        self.draw_figure_x()
+        self.graph_y.get_tk_widget().pack_forget()
+        self.graph_y.get_tk_widget().destroy()
+        self.draw_figure_y()
+
+        self.x.clear()
+        self.y.clear()
+        self.number_of_events.clear()
+
+    def read_file(self):
         # open file for read
         file_handler = open(
             "e-BioDigit_DB/" + self.folder_name.get() + "/" + self.session_name.get() + "/" + self.file_name.get())
@@ -148,20 +236,27 @@ class Application:
                 self.y.append(int(line.split()[1]))
         # close file
         file_handler.close()
-        # remove graphs and redraw
-        self.graph_number.get_tk_widget().pack_forget()
-        self.graph_number.get_tk_widget().destroy()
-        self.draw_figure_number()
-        self.graph_x.get_tk_widget().pack_forget()
-        self.graph_x.get_tk_widget().destroy()
-        self.draw_figure_x()
-        self.graph_y.get_tk_widget().pack_forget()
-        self.graph_y.get_tk_widget().destroy()
-        self.draw_figure_y()
 
-        self.x.clear()
-        self.y.clear()
-        self.number_of_events.clear()
+    def compare_read_file(self):
+        # open file for read
+        file_handler = open(
+            "e-BioDigit_DB/" + self.compare_folder_name.get() + "/" + self.compare_session_name.get() + "/" + self.compare_file_name.get())
+        # read file lines
+        lines = file_handler.read().splitlines()
+        # go through file lines
+        for line in lines:
+            # if first row
+            if len(line.split()) == 1:
+                self.compare_number_of_events.append(int(line.split()[0]))
+            else:
+                self.compare_x.append(int(line.split()[0]))
+                self.compare_y.append(int(line.split()[1]))
+        # close file
+        file_handler.close()
+
+    def dtw_calculation(self):
+        euclidean_norm = lambda x, y: np.abs(x - y)
+        d, cost_matrix, acc_cost_matrix, path = dtw(self.x, self.y, dist=euclidean_norm)
 
 
 if __name__ == '__main__':
